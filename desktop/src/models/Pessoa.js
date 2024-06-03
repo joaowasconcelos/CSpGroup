8b82a72df5fb3ab7502cbee184a4c71879728854
Your Name <you@example.com>
2024-06-03 19:25:32 -0300

    validacao CPF

--- desktop/src/models/Pessoa.js
+++ desktop/src/models/Pessoa.js
@@ -27,19 +27,44 @@ class Pessoa {
 
 
     DataConvert(value) {
-        let [dia, mes, ano] = value.split('/'); 
+        let [dia, mes, ano] = value.split('/');
         let dataFormatada = `${ano}-${mes}-${dia}`;
         this.dataNasc = dataFormatada;
     }
 
-    validaCampos(){
-        return(
-            this.cpf&&
-            this.nome&&
-            this.dataNasc&&
-            this.genero&&
+    validaCampos() {
+        return (
+            this.nome &&
+            this.dataNasc &&
+            this.genero &&
             this.email
         )
+    }
+
+    validarCPF(cpf) {
+        var Soma;
+        var Resto;
+        Soma = 0;
+        if (cpf == "00000000000") return false + " CPF invalido!";
+
+        for (i = 1; i <= 9; i++) {
+            Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+        }
+        Resto = (Soma * 10) % 11;
+        console.log(Resto);
+
+        if ((Resto == 10) || (Resto == 11)) Resto = 0;
+        if (Resto != parseInt(cpf.substring(9, 10))) return false + " CPF invalido!";
+
+        Soma = 0;
+        for (i = 1; i <= 10; i++) {
+            Soma = Soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+        }
+        Resto = (Soma * 10) % 11;
+
+        if ((Resto == 10) || (Resto == 11)) Resto = 0;
+        if (Resto != parseInt(cpf.substring(10, 11))) return false + " CPF invalido!";
+        return true + " CPF valido!";
     }
 }
 
