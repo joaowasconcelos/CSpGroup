93b429c317e6f14091f268d43ff61216a79403cc
Thais <thaisachmidtdasilva@gmail.com>
2024-06-03 21:44:41 -0300

    feita as navegações entre telas - mobile 03.06.2024

--- mobile/mobile/App.js
+++ mobile/mobile/App.js
@@ -1,39 +1,101 @@
 import * as React from 'react';
-import { Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { NavigationContainer } from '@react-navigation/native';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import Home from '../mobile/src/pages/Home';
+import Consultas from '../mobile/src/pages/Consultas';
+import Contato from '../mobile/src/pages/Contato';
+import Dados from '../mobile/src/pages/Dados';
+import Login from '../mobile/src/pages/Login';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator();
+
+function MainTabs() {
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        tabBarActiveTintColor: '#004c4c',
+        tabBarInactiveTintColor: 'white',
+        tabBarStyle: {
+          backgroundColor: '#b4c4bc',
+        },
+      }}
+    >
+      <Tab.Screen
+        name='Home'
+        component={Home}
+        options={{
+          title: 'Home',
+          headerTintColor: '#053c20',
+          headerStyle: {
+            backgroundColor: '#b4c4bc',
+          },
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name='home' color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Consultas'
+        component={Consultas}
+        options={{
+          title: 'Consultas',
+          headerStyle: {
+            backgroundColor: '#b4c4bc',
+          },
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name='id-card' color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name='Contato'
+        component={Contato}
+        options={{
+          title: 'Contato',
+          headerTintColor: '#053c20',
+          headerStyle: {
+            backgroundColor: '#b4c4bc',
+          },
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome name='phone' color={color} size={size} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
+
+function LoginStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Main"
+        component={MainTabs}
+        options={{ headerShown: false }}
+      />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    
-    <NavigationContainer>
-
-      <Tab.Navigator>
-        
-        <Tab.Screen name="Home" component={HomeScreen} options={{headerShown: false}} />
-
-        <Tab.Screen name="Settings" component={SettingsScreen} options={{headerShown: false}} />
-
-      </Tab.Navigator>
-
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <LoginStack />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
---
+++ mobile/mobile/src/pages/Consultas/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Consultas = () => {
+  return (
+    <View>
+      <Text>Consultas Screen</Text>
+    </View>
+  );
+};
+
+export default Consultas;
