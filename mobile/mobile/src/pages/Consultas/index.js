da9e1ab7166c616bdff6cc18733455c78c758c51
Thais <thaisachmidtdasilva@gmail.com>
2024-06-04 21:46:34 -0300

    mobile - tela de login em andamento

--- mobile/mobile/src/pages/Consultas/index.js
+++ mobile/mobile/src/pages/Consultas/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { SafeAreaView, ScrollView, Platform, StyleSheet, Text, View, Button } from 'react-native';
 
 const Consultas = () => {
   return (
-    <View>
-      <Text>Consultas Screen</Text>
-    </View>
+
+    <SafeAreaView style={styles.androidSafeArea}>
+
+      <ScrollView>
+
+        <View style={styles.container}>
+
+          <Text>Consultas Screen</Text>
+
+        </View>
+
+      </ScrollView>
+
+    </SafeAreaView>
   );
 };
 
 export default Consultas;
+
+const styles = StyleSheet.create({
+  androidSafeArea: {
+    paddingTop: Platform.OS === 'android' ? 40 : 0,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#f1f1f1',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 897
+  },
+});
+
+
