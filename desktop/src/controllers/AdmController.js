c08ba83ac4634a909a17df446f7787d192d94356
thiagoantonio019 <thiago_antonio05@hotmail.com>
2024-06-13 21:48:02 -0300

    alteração front

--- desktop/src/controllers/AdmController.js
+++ desktop/src/controllers/AdmController.js
@@ -12,6 +12,28 @@ const viewAdm = {
 
     },
 
+    paginaConsultaAdm: async (req, res) => {
+        try {
+            res.render('pages/ConsultaAdm');
+        }
+        catch (error) {
+            console.log(error);
+            res.render('pages/pag_erro', { message: error });
+        }
+
+    },
+
+
+    paginaMedicoAdm: async(req, res) => {
+        try{
+            res.render('pages/MedicoAdm');
+        }
+        catch (error) {
+            console.log(error);
+            res.render('pages/pag_erro', { message: error});
+        }
+    }
+
 }
 
 module.exports = { viewAdm }
--- desktop/src/routes/clinicaRoutes.js
+++ desktop/src/routes/clinicaRoutes.js
@@ -6,8 +6,9 @@ const { LoginPerfis } = require("../controllers/loginController")
 const {cadastroEspecia} = require("../controllers/cadastroEspecialidade");
 const {cadastroConsulta} = require("../controllers/cadastroConsulta");
 const { viewPaciente } = require("../controllers/pacienteController");
-const { viewMedico }= require("../controllers/MedicoController")
+const { viewMedico }= require("../controllers/MedicoController");
 const { viewAdm} = require("../controllers/AdmController")
+
 
 router.get('/', (req, res) => {
     res.render('index', { title: 'Página Inicial' });
@@ -17,21 +18,23 @@ router.post("/Cadastro", LoginPerfis.LoginPessoa);
 router.post("/Pessoa/novo",cadastro.adicionaPessoa);
 router.post ("/Cadastro/Especialidade",cadastroEspecia.cadastraEspecialidade)
 router.post ("/Cadastro/Consulta",cadastroConsulta.cadastraConsulta)
-router.get ("/Login",LoginPerfis.paginaLogin) /*Direciona para a pagina de login*/
 router.post("/Login/entrar",LoginPerfis.LoginPessoa);
 router.get('/login/loginTipo', LoginPerfis.selecionaTipo);
 router.get("/paciente/infos",viewPaciente.selecionaInfosPaciente);
-router.get ("/Consulta",cadastroConsulta.paginaConsulta) /*Direciona para a pagina de login*/
-
 router.get("/paciente/consultas", viewPaciente.selecionaConsultas);
 
-router.get ("/Paciente",viewPaciente.paginaPaciente) /*Direciona para a pagina de login*/
-router.get ("/Medico",viewMedico.paginaMedico) /*Direciona para a pagina de login*/
+
+router.get ("/MedicoAdm", viewAdm.paginaMedicoAdm) /*Direciona para a pagina de vizualiçoes dos medicos*/
+router.get ("/Login",LoginPerfis.paginaLogin) /*Direciona para a pagina de login*/
+router.get ("/Consulta",cadastroConsulta.paginaConsulta) /*Direciona para a pagina de consulta*/
+router.get ("/Paciente",viewPaciente.paginaPaciente) /*Direciona para a pagina de paciente*/
+router.get ("/Medico",viewMedico.paginaMedico) /*Direciona para a pagina de medico*/
 router.get ("/Adm",viewAdm.paginaAdm) /*Direciona para a pagina de adm*/
+router.get ("/ConsultaAdm",viewAdm.paginaConsultaAdm) /*Direciona para ver a pagona consulta*/
+
 router.use(function (req, res) {
     res.status(404).render(`pages/pag_erro`, { message: '404 - Página não encontrada' })
 })
-
 
 
 module.exports = router;
